e3b4a4f8cd873a027fd32d159f1efd6201be5684
asingizwe1 <asingizwe1louis1@gmail.com>
2026-06-13 01:08:17 +0300

    fixing un reachable _ match

--- src/lib.rs
+++ src/lib.rs
@@ -75,8 +75,7 @@ impl CompactSize {
                     bytes[1], bytes[2], bytes[3], bytes[4], bytes[5], bytes[6], bytes[7], bytes[8],
                 ]);
                 Ok((CompactSize::new(val), 9))
-            } // _ => Err(BitcoinError::InvalidFormat),
-              // -> can never be reached since all possible u8 values are reached
+            }
         }
     }
 }
